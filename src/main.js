4855c4a26228f644d3984115997c80994d216fcb
Rhys Laval <rhyslaval@gmail.com>
2019-08-07 16:18:41 +0100

    Update main with ElementUI and VueX Imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
+
+// configure vuex here
 
 new Vue({
   render: h => h(App),
